0a38b63a60137e51958aecd1dc577467fc2343c6
Saleh Zetawi <salehzetawi15@gmail.com>
2024-01-06 10:55:58 +0200

    create first user by query builder in DBseeder

---
+++ app/Models/Topic.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Topic extends Model
+{
+    use HasFactory;
+
+/**    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+    protected $connection='mysql';
+
+    protected $table='topics';
+    protected $primaryKey='id';
+    protected $keyType='int';
+    public $incrementing=true;
+ */
+    public $timestamps=false;
+
+
+}
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Query Builder
+
+        DB::table('users')->insert([
+            'name'=>'saleh zetawi',
+            'email'=>'s@saleh.ps',
+            'password'=>Hash::make('password'),
+        ]);
+
     }
 }
